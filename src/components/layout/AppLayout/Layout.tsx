0795425621f220461a7e8c652fdba57957bbce3e
Victor Karanja <vic@vappsio.com>
2022-02-10 02:02:19 +0300

    feat: init layout component

---
+++ src/components/layout/AppLayout/Layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LayoutWrapper from './components/LayoutWrapper';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => (
+  <LayoutWrapper>{children}</LayoutWrapper>
+);
+
+export default Layout;
