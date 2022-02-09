3bb395ed89dcceaf629f37239f3f4f50fee658ac
Victor Karanja <vic@vappsio.com>
2022-02-10 01:38:28 +0300

    feat: adding in global types

---
+++ src/types.ts
@@ -0,0 +1,28 @@
+export interface Image {
+  src: string;
+  alt: string;
+}
+
+export interface Product {
+  id: number;
+  name: string;
+  image: Image;
+  price: number;
+  orginalPrice?: number;
+}
+
+export type Products = Product[];
+
+export interface KeyValPair {
+  [v: string]: any;
+}
+
+export enum Currency {
+  USD = '$',
+}
+
+export interface CartItem extends Product {
+  quantity: number;
+}
+
+export type CartItems = CartItem[];
