fbbc738f5e9baec65181a18627efe34f8241637b
Victor Karanja <vic@vappsio.com>
2022-02-10 01:40:47 +0300

    feat: init global cart component

---
+++ src/components/common/Cart/Cart.tsx
@@ -0,0 +1,12 @@
+import { CartItems } from '@/types';
+import React from 'react';
+
+interface CartProps {
+  items: CartItems;
+}
+
+const Cart = ({}: CartProps) => {
+  return <div>Cart</div>;
+};
+
+export default Cart;
