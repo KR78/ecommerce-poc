59f77aadebf156dde152f2073ef84ee76b934ce4
Victor Karanja <vic@vappsio.com>
2022-02-10 01:53:42 +0300

    feat: removing boilerplate, adding in font, header meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,40 @@
-import { AppProps } from 'next/app';
-import '@/styles/global.css';
+/* eslint-disable @next/next/no-page-custom-font */
+import React from 'react';
+import Head from 'next/head';
+import Layout from '@/components/layout/AppLayout';
+import '../scss/globals.scss';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const App = ({
+  Component,
+  pageProps,
+}: {
+  Component: any;
+  pageProps: { [v: string]: unknown };
+}) => (
+  <>
+    <Head>
+      <meta charSet="utf-8" />
+      <meta name="robots" content="noindex, nofollow" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, viewport-fit=cover"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Archivo:wght@300;400;500;600&family=Sora:wght@400;500;600&display=swap"
+        rel="stylesheet"
+      />
+      <title>Ecommerce PoC</title>
+      <meta name="description" content="Ecommerce PoC"></meta>
+      <link
+        rel="icon"
+        type="image/png"
+        href="https://bejamas.io/favicon-32x32.png"
+      ></link>
+    </Head>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  </>
+);
+
+export default App;
