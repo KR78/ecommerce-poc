40af1a378671ee3aa83863c9655f060afd17be07
Victor Karanja <vic@vappsio.com>
2022-02-10 01:49:09 +0300

    feat: init app layout with apollo client, hasura and cart provider

---
+++ src/components/layout/AppLayout/components/LayoutWrapper/LayoutWrapper.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import CartProvider from 'src/components/providers/cartProvider';
+import Main from '../Main';
+import NavBar from '../NavBar';
+
+interface LayoutWrapperProps {
+  children: React.ReactNode;
+}
+
+const client = new ApolloClient({
+  uri: 'https://mint-moth-74.hasura.app/v1/graphql',
+  cache: new InMemoryCache(),
+  headers: {
+    'x-hasura-admin-secret':
+      '45rHkPeCPSZUhcpvcpEyPyrFT0mHGbpd6NT8Cv74FVzYScLOqPA5sWeYE9Y2Eqq3',
+  },
+});
+
+const LayoutWrapper = ({ children }: LayoutWrapperProps) => (
+  <ApolloProvider client={client}>
+    <CartProvider>
+      <NavBar />
+      <Main>{children}</Main>
+    </CartProvider>
+  </ApolloProvider>
+);
+
+export default LayoutWrapper;
