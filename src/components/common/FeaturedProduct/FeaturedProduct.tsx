2a96524739044c80786aabb9bcfc967b1d896235
Victor Karanja <vic@vappsio.com>
2022-02-10 15:21:35 +0300

    feat: init working featured product

--- src/components/common/FeaturedProduct/FeaturedProduct.tsx
+++ src/components/common/FeaturedProduct/FeaturedProduct.tsx
@@ -1,10 +1,56 @@
 import React from 'react';
+import { Product } from '@/types';
+import Button from '../Button';
+import ImageHolder from '../ImageHolder';
+import Recommendations from '../Recommendations';
+import ProductDetails from '../ProductDetails';
 import style from './FeaturedProduct.module.scss';
 
-const FeaturedProduct = () => {
+interface FeaturedProductProps {
+  product: Product,
+}
+
+const FeaturedProduct = ({
+  product,
+}: FeaturedProductProps) => {
   return (
-    <article>
-      Featured
+    <article className={style.wrapper}>
+      <header className={style.header}>
+        {product?.name || ''}
+      </header>
+      <ImageHolder
+        featured
+        image={product?.image}
+        placeHolderSrc="https://picsum.photos/385/239"
+      />
+      <Button
+        className={style.addToCartBtn}
+        onClick={() => null}
+      >
+        Add to Cart
+      </Button>
+      <article className={style.productOverview}>
+        <section className={style.productDescription}>
+          <header className={style.header}>
+            <span>About the&nbsp;</span>
+            <span className={style.productName}>
+              {product?.name || ''}
+            </span>
+          </header>
+          <p className={style.description}>
+            {product?.details?.description || ''}
+          </p>
+        </section>
+        <section>
+          <Recommendations
+            products={product?.details?.recommendations || []}
+          />
+          <ProductDetails
+            size={product?.details?.size || 0}
+            dimensions={product?.details?.dimensions || {width: 0, height: 0}}
+          />
+        </section>
+      </article>
     </article>
   );
 };
