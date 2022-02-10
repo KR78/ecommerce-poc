41812ec125a6e567806a38e827d072cd740f1963
Victor Karanja <vic@vappsio.com>
2022-02-10 23:55:07 +0300

    feat: adding in filterComponent to sit on top when opened

--- src/components/layout/AppLayout/components/Main/Main.tsx
+++ src/components/layout/AppLayout/components/Main/Main.tsx
@@ -1,20 +1,41 @@
 import React from 'react';
 import Cart from '@/components/common/Cart';
 import { useCartProvider } from 'src/components/providers/cartProvider';
-
+import { useFilterProvider } from 'src/components/providers/filterProvider';
+import FilterComponent from '@/components/common/FilterComponent';
+import style from './Main.module.scss';
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const Main = ({ children }: LayoutProps) => {
   const { cart } = useCartProvider();
+  const { isFilterOpen } = useFilterProvider();
+
+  console.log({
+    isFilterOpen,
+    useCartProvider: useCartProvider(),
+    useFilterProvider: useFilterProvider(),
+  })
 
   return (
     <>
-      <main>
+      <main
+        className={
+          isFilterOpen
+            ? style.filterIsOpen
+            : ''
+          }
+      >
         {children}
       </main>
       <Cart items={cart} />
+      {
+        isFilterOpen
+        && (
+          <FilterComponent />
+        )
+      }
     </>
   );
 };
