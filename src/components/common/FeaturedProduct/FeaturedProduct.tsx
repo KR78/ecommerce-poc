eb1b3cfa31ec59417937e56fdbda4ca4264840ec
Victor Karanja <vic@vappsio.com>
2022-02-10 03:22:37 +0300

    feat: init FeaturedProduct component

---
+++ src/components/common/FeaturedProduct/FeaturedProduct.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './FeaturedProduct.module.scss';
+
+const FeaturedProduct = () => {
+  return (
+    <article>
+      Featured
+    </article>
+  );
+};
+
+export default FeaturedProduct;
