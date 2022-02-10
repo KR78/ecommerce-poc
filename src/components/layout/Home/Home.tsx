42ee731b9c9a8cb76f53af7df314e82805ee6dba
Victor Karanja <vic@vappsio.com>
2022-02-10 15:27:15 +0300

    feat: importing featured prodcut component

--- src/components/layout/Home/Home.tsx
+++ src/components/layout/Home/Home.tsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import { Product } from '@/types';
+import getProducts from '@/service/getProducts';
+import FeaturedProduct from '@/components/common/FeaturedProduct';
 
 const Home = () => {
-  return <div>Home</div>;
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [featuredProduct, setFeaturedProduct] = React.useState<Product>();
+  const { loading, error, data } = getProducts();
+
+  React.useEffect(() => {
+    if (loading) return setIsLoading(loading);
+
+    if (data && data.products) {
+      const {
+        products,
+      } = data;
+      const getFeaturedProduct = products.filter((product) => product?.featured)[0];
+      console.log({
+        data,
+        getFeaturedProduct,
+      });
+      setFeaturedProduct(getFeaturedProduct);
+    }
+
+  }, [loading, error, data])
+
+  return (
+    <article>
+      {
+        featuredProduct
+        && (
+          <FeaturedProduct
+            product={featuredProduct}
+          />
+        )
+      }
+    </article>
+  );
 };
 
 export default Home;
