a09ba7d329550406db9efd647594efda5a41f4c8
Victor Karanja <vic@vappsio.com>
2022-02-10 15:11:14 +0300

    feat: init working Product Details

--- src/components/common/ProductDetails/ProductDetails.tsx
+++ src/components/common/ProductDetails/ProductDetails.tsx
@@ -1,5 +1,6 @@
 import { Dimensions } from '@/types';
 import React from 'react';
+import convertKBToMB from './helpers/convertKbToMB';
 import style from './ProductDetails.module.scss';
 
 interface ProductDetailsProps {
@@ -12,8 +13,31 @@ const ProductDetails = ({
   dimensions,
 }: ProductDetailsProps) => {
   return (
-    <article>
-      Details
+    <article className={style.wrapper}>
+      <header className={style.header}>
+        Details
+      </header>
+      <section className={style.details}>
+        {
+          dimensions
+          && dimensions?.width > 0
+          && dimensions?.height > 0
+          && (
+            <span>
+              {`Size: ${dimensions.width} x ${dimensions.height} pixel`}
+            </span>
+          )
+        }
+        {
+          size
+          && size > 0
+          && (
+            <span>
+              {`Size: ${convertKBToMB(size)} mb`}
+            </span>
+          )
+        }
+      </section>
     </article>
   );
 };
