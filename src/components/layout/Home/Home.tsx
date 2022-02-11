7b5a212caf3a059b2749e5d29160d468b5104efc
Victor Karanja <vic@vappsio.com>
2022-02-11 14:40:58 +0300

    feat: adding in product listing component

--- src/components/layout/Home/Home.tsx
+++ src/components/layout/Home/Home.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Product } from '@/types';
+import {
+  Product,
+  Products,
+} from '@/types';
 import getProducts from '@/service/getProducts';
 import FeaturedProduct from '@/components/common/FeaturedProduct';
 import Button from '@/components/common/Button';
 import BreadCrumb from '@/components/common/BreadCrumb';
 import { useFilterProvider } from '@/components/providers/filterProvider';
+import ProductsList from '@/components/common/ProductsList';
 import style from './Home.module.scss';
 
 const Home = () => {
   const [isLoading, setIsLoading] = React.useState(true);
+  const [products, setProducts] = React.useState<Products>();
   const [featuredProduct, setFeaturedProduct] = React.useState<Product>();
 
   const { setisFilterOpen } = useFilterProvider();
@@ -21,12 +26,13 @@ const Home = () => {
       const {
         products,
       } = data;
+
+      setProducts(products);
+
       const getFeaturedProduct = products.filter((product) => product?.featured)[0];
-      console.log({
-        data,
-        getFeaturedProduct,
-      });
+
       setFeaturedProduct(getFeaturedProduct);
+      setIsLoading(false);
     }
 
   }, [loading, error, data])
@@ -58,6 +64,17 @@ const Home = () => {
               <img src="icons/filter.svg" alt="Filter Icon" />
             </span>
           </Button>
+        </section>
+        <section>
+          {
+            products
+            && products.length > 0
+            && (
+              <ProductsList
+                products={products}
+              />
+            )
+          }
         </section>
       </article>
     );
